fc1d8456bae370a5625d5e26a066412c5b5e3b01
Etvulk <ian115sugi@mgmail.com>
2023-04-13 14:50:34 -0400

    aaaa

--- Blackjack.java
+++ Blackjack.java
@@ -21,6 +21,7 @@ enum Carta{
 public class Blackjack{
 
     public static void main(String[] args){
+        MaquinaDeTrazados mt = new MaquinaDeTrazados(1400, 700, "JUEGO", Colores.DARK_GRAY);
 
         Carta[] mazo = {
             Carta.DIAMANTE_AS, Carta.TREBOL_AS, Carta.CORAZON_AS, Carta.PICAS_AS,
@@ -51,6 +52,7 @@ public class Blackjack{
         int apuestaMinima = 10;
         int juegos = 0;
         int juegosMaximos = 10;
+        int[] accionesJugador = new int[4];
         boolean continuarJuego = true;
 
         System.out.println("Bienvenido a Blackjack!");
@@ -61,7 +63,7 @@ public class Blackjack{
 
         while (juegos < juegosMaximos && creditos >= apuestaMinima && continuarJuego){
             System.out.println("Tienes " + creditos + " créditos.");
-            int apuesta = perdirApuesta(creditos);
+            int apuesta = perdirApuesta(creditos, jugador);
             creditos = creditos - apuesta;
 
             Carta[] manoJugador = new Carta[21];
@@ -85,15 +87,27 @@ public class Blackjack{
             System.out.println("La primera carta del crupier es: ");
             System.out.println(manoCrupier[0]);
             System.out.println("Y su valor es: " + manoValorCrupier);
+            manoValorCrupier = valorCartas(manoCrupier, numeroCartas, 2);
 
-            MaquinaDeTrazados mt = new MaquinaDeTrazados(1400, 700, "JUEGO", Colores.DARK_GRAY);
+            //MaquinaDeTrazados mt = new MaquinaDeTrazados(1400, 700, "JUEGO", Colores.DARK_GRAY);
             dibujarCartasJugador(mt, manoJugador, 2);
-            dibujarCartasCrupier(mt, manoCrupier, 2);
+            dibujarCartasCrupier(mt, manoCrupier, 2, 1);
             mt.mostrar();
             
-            leer.nextLine();
-            continuarJuego = false;
+            
+            accionesJugador = accionesJugador(manoJugador, manoCrupier, mazo, numeroCartas, jugador, 2, creditos, apuesta, mt);
+            manoValorJugador = accionesJugador[0];
+            creditos = accionesJugador[1];
+            apuesta = accionesJugador[2];
+            int seguirMano = accionesJugador[3];
+            manoValorCrupier = accionesCrupier(manoCrupier, mazo, numeroCartas, 2, seguirMano, manoValorCrupier, mt);
+            creditos = decisionDelJuego(manoValorJugador, manoValorCrupier, creditos, apuesta, seguirMano);
+            continuarJuego = seguirJugando(seguirMano);
+            mt.limpiar();
+            mt.terminar();
+            
         }
+        mt.terminar();
         leer.close();
         
     }
@@ -111,25 +125,32 @@ public class Blackjack{
         return mano;
     }
 
+    public static Carta[] agregarCarta(Carta[] mano, Carta[] mazo, int cantidaDeCartas){
+        int cartasMazo = mazo.length;
+        int carta = (int) (Math.random() * cartasMazo);
+        mano[cantidaDeCartas] = mazo[carta];
+        return mano;
+    }
+
     // Método para determinar si la apuesta es valida.
-    public static int perdirApuesta(int creditos){ 
+    public static int perdirApuesta(int creditos, String jugador){ 
         int i = 0;
         Scanner leer = new Scanner(System.in);
-        System.out.println("Introduza la cantidad de créditos que desea apostar(la cantidad mínima de créditos que se pueden apostar es 10):");
+        System.out.println(jugador + " por favor introduza la cantidad de créditos que desea apostar(la cantidad mínima de créditos que se pueden apostar es 10):");
         int apuesta = leer.nextInt();
         // Loop para asegurar que la apuesta es válida.
-        while ((apuesta < 10 && i < 10) || apuesta > creditos){
+        while ((apuesta < 10 && i < 5) || apuesta > creditos){
             System.out.println("Apuesta inválida, por favor ingresé una apuesta mayor o igual a 10 créditos:");
             apuesta = leer.nextInt();
             i++;
         }
-        // Si la cantidad de intentos es mas de 10, entonces se le asignara una apuesta de 10 créditos al jugador.
-        if (i >= 10){
+        // Si la cantidad de intentos es mas de 5, entonces se le asignara una apuesta de 10 créditos al jugador.
+        if (i >= 5){
             apuesta = 10;
             System.out.println("Cantidad de intentos excedida, su apuesta fue asignada a 10 créditos.");
         }
         // Cerrar escaner.
-        leer.close(); 
+        
         return apuesta;
 
     }
@@ -149,6 +170,190 @@ public class Blackjack{
         }
         return valor;
     }
+
+    public static boolean esBlackJack(Carta[] manoJugador, Carta[] manoCrupier, int[] numeroCartas){
+        int valorManoCrupier = valorCartas(manoCrupier, numeroCartas, 2);
+        int valorManoJugador = valorCartas(manoJugador, numeroCartas, 2);
+
+        if (valorManoJugador == 21 && valorManoCrupier != 21){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+    // Método que se encarga de la toma de decisiones del jugador.
+    public static int[] accionesJugador(Carta[] mano, Carta[] manoCrupier, Carta[] mazo, int[] numeroCartas, String jugador, int cantidaDeCartas, int creditos, int apuesta, MaquinaDeTrazados mt){
+        int valorCartas = valorCartas(mano, numeroCartas, cantidaDeCartas);
+        boolean seguirJugando = true;
+        Scanner leer = new Scanner(System.in);
+        int[] datosJugador = new int[4];
+        boolean esBlackJack = esBlackJack(mano, manoCrupier, numeroCartas);
+        if (esBlackJack == true){
+            System.out.println("Felicidades, ganaste con BlackJack.");
+            int creditosGanados = apuesta + (apuesta * 3)/2;
+            System.out.println("Has ganado " + creditosGanados + " créditos.");
+            creditos = creditos + apuesta + (apuesta *3) /2;
+
+            datosJugador[0] = valorCartas;
+            datosJugador[1] = creditos;
+            datosJugador[2] = apuesta;
+            datosJugador[3] = 0;
+            return datosJugador;
+        } 
+        while (seguirJugando == true){
+            if (valorCartas <= 21){
+                System.out.println(jugador + ", por favor introduzca el número de alguna de las siguientes opciones para decidir que hacer en su turno:");
+                System.out.println("1) Agregar carta a la mano.");
+                System.out.println("2) Plantarse.");
+                System.out.println("3) Doblar apuesta.");
+                System.out.println("4) Salir del juego.");
+                System.out.println("Introduzca el número de la opción que desea realizar:");
+                int opcion = leer.nextInt();
+
+                if (opcion == 1){
+                    mano = agregarCarta(mano, mazo, cantidaDeCartas);
+                    System.out.println("La carta agregada a su mano es: " + mano[cantidaDeCartas] + " y su válor es de " + numeroCartas[mano[cantidaDeCartas].ordinal()]);
+                    cantidaDeCartas++;
+                    dibujarCartasJugador(mt, mano, cantidaDeCartas);
+                    mt.repintar();
+                    valorCartas = valorCartas(mano, numeroCartas, cantidaDeCartas);
+                    System.out.println("El válor de su mano es: " + valorCartas);
+                }
+                else if (opcion == 2){
+                    seguirJugando = false;
+                }
+
+                else if (opcion == 3){
+                    if ((valorCartas == 9 || valorCartas == 10 || valorCartas == 11) && (cantidaDeCartas == 2)){
+                        creditos = creditos - apuesta;
+                        apuesta = apuesta * 2;
+                        
+                    }
+                    else{
+                        System.out.println("No es posible doblar la apuesta.");
+                    }
+                }
+
+                else if (opcion == 4){
+                    datosJugador[0] = valorCartas;
+                    // créditos.
+                    datosJugador[1] = 0;
+                    datosJugador[2] = apuesta;
+                    // seguirMano.
+                    datosJugador[3] = -1;
+                    seguirJugando = false;
+                    return datosJugador;
+                }
+
+                else{
+                    System.out.println("Opción invalida, por favor introduzca un número correspondiente a las opciones dadas.");
+                }
+            
+            }
+            else if (valorCartas > 21){
+                System.out.println(jugador + ", el válor de sus cartas ha sobrepasado 21, perdió el juego.");
+                System.out.println("Has perdido " + apuesta + " créditos.");
+                datosJugador[0] = valorCartas;
+                datosJugador[1] = creditos;
+                datosJugador[2] = apuesta;
+                datosJugador[3] = 0;
+                seguirJugando = false;
+                return datosJugador;
+            }
+            
+        }
+        datosJugador[0] = valorCartas;
+        datosJugador[1] = creditos;
+        datosJugador[2] = apuesta;
+        datosJugador[3] = 1;
+        return datosJugador;
+    }
+
+    // Método para las acciones del crupier.
+    public static int accionesCrupier(Carta[] mano, Carta[] mazo, int[] numeroCartas, int cantidaDeCartas, int seguirMano, int valorCartas, MaquinaDeTrazados mt){
+        if (seguirMano == 1){
+            while (valorCartas < 17){
+                mano = agregarCarta(mano, mazo, cantidaDeCartas);
+                cantidaDeCartas++;
+                dibujarCartasCrupier(mt, mano, cantidaDeCartas, cantidaDeCartas);
+                mt.repintar();
+                valorCartas = valorCartas(mano, numeroCartas, cantidaDeCartas);
+            }
+            
+            int i = 0;
+            System.out.println("Las cartas del crupier son:");
+            while (i < cantidaDeCartas){
+                System.out.println(mano[i]);
+                i++;
+            }
+            System.out.println("El válor de la mano del crupier es: " + valorCartas + ".");
+            return valorCartas;
+        }
+        else{
+            return valorCartas;
+        }
+    }
+
+    public static int decisionDelJuego(int valorManoJugador, int valorManoCrupier, int creditos, int apuesta, int seguirMano){
+        if (seguirMano == 1){
+            if (valorManoCrupier > 21){
+                System.out.println("El válor de las cartas del crupier es mayor a 21, por lo tanto has ganado.");
+                creditos = creditos + apuesta + apuesta;
+                System.out.println("Has ganado " + apuesta + " créditos.");
+                return creditos;
+            }
+
+            else if (valorManoJugador == valorManoCrupier){
+                System.out.println("El válor de su mano y la del crupier son iguales, por lo tanto es un empate.");
+                creditos = creditos + apuesta;
+                System.out.println("Se le devolverá la apuesta realizada.");
+                return creditos;
+            }
+
+            else if (valorManoJugador > valorManoCrupier){
+                System.out.println("El válor de su mano es mayor a la del crupier, por lo tanto has ganado.");
+                creditos = creditos + apuesta + apuesta;
+                System.out.println("Has ganado " + apuesta + " créditos.");
+                return creditos;
+            }
+
+            else if (valorManoJugador < valorManoCrupier){
+                System.out.println("El válor de su mano es menor a la del crupier, por lo tanto has perdido.");
+                System.out.println("Has perdido " + apuesta + " créditos.");
+            }
+            return creditos;
+        }
+        else{
+            return creditos;
+        }
+    }
+
+    public static boolean seguirJugando(int seguirMano){
+        if (seguirMano != -1){
+            Scanner leer = new Scanner(System.in);
+            System.out.println("¿Desea seguir jugando?(escriba '1' para continuar o '0' para salir del juego)");
+            int opcion = leer.nextInt();
+            int i = 0;
+            while (i < 5){
+                if (opcion == 1 ){
+                    return true;
+                }
+                else if (opcion == 0){
+                    return false;
+                }
+                else{
+                    System.out.println("Por favor introduzca una opción válida.");
+                    opcion = leer.nextInt();
+                }
+                i++;
+            }
+            return true;
+        }
+        return false;
+    }
+
+    
     // Método para dibujar las cartas del jugador.
     public static void dibujarCartasJugador(MaquinaDeTrazados mt, Carta[] mano, int cantidaDeCartas){
         String[] simbolo;
@@ -181,9 +386,9 @@ public class Blackjack{
             "8", "8", "8", "8",
             "9", "9", "9", "9",
             "10", "10", "10", "10",
-            "10", "10", "10", "10",
-            "10", "10", "10", "10",
-            "10", "10", "10", "10",
+            "J", "J", "J", "J",
+            "Q", "Q", "Q", "Q",
+            "K", "K", "K", "K",
             "JOKER", "JOKER", "JOKER", "JOKER"
         };
         mt.configurarFuente("Monospaced", Font.PLAIN, 24);
@@ -220,7 +425,7 @@ public class Blackjack{
         }
     }
     // Método para dibujar las cartas del crupier.
-    public static void dibujarCartasCrupier(MaquinaDeTrazados mt, Carta[] mano, int cantidaDeCartas){
+    public static void dibujarCartasCrupier(MaquinaDeTrazados mt, Carta[] mano, int cantidaDeCartas, int cartasDesveladas){
         String[] simbolo;
         simbolo = new String[]{
             "DIAMANTE", "TREBOL", "CORAZON", "PICAS",
@@ -251,9 +456,9 @@ public class Blackjack{
             "8", "8", "8", "8",
             "9", "9", "9", "9",
             "10", "10", "10", "10",
-            "10", "10", "10", "10",
-            "10", "10", "10", "10",
-            "10", "10", "10", "10",
+            "J", "J", "J", "J",
+            "Q", "Q", "Q", "Q",
+            "K", "K", "K", "K",
             "JOKER", "JOKER", "JOKER", "JOKER"
         };
         mt.configurarFuente("Monospaced", Font.PLAIN, 24);
@@ -263,7 +468,7 @@ public class Blackjack{
         int centroY = y/17;
         int i = 0;
         int separador = 0;
-        while (i < 1){
+        while (i < cartasDesveladas){
             mt.dibujarRectanguloLleno(centroX + separador, centroY, 150, 200, Colores.WHITE);
             mt.dibujarRectangulo(centroX + separador, centroY, 150, 200, Colores.RED);
             if (simbolo[mano[i].ordinal()] == "DIAMANTE"){
